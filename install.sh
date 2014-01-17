87cf8d11e2fcb60dc242a4dca7610d71f166aab6
Ricardo Lipas Augusto <ricardo.lipas@gmail.com>
2014-01-17 18:18:39 -0200

    Added a simple install script and some tweaks.

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Run this script on your home directory!
+
+DIR=`dirname $0`
+
+cd
+
+# .bashrc
+ln -sf $DIR/bash/bashrc .bashrc
+
+# .gitconfig
+ln -sf $DIR/git/gitconfig .gitconfig
+
+# .pystartup
+ln -sf $DIR/python/pystartup .pystartup
+
+#.inputrc
+ln -sf $DIR/readline/inputrc .inputrc
+
+#.vim + .vimrc
+ln -sf $DIR/vim/ .vim
+ln -sf $DIR/vim/vimrc .vimrc
+
+#.tmux.conf
+ln -sf $DIR/tmux/tmux.conf .tmux.conf
+
+#.dir_colors
+ln -sf $DIR/dircolors-solarized/dircolors.256dark .dir_colors
